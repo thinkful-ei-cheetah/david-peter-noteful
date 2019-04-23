f5d975d3ea43366509520cfc2346a103012d67ff
Peter Pae <paendabear@gmail.com>
2019-04-23 10:53:00 -0400

    add dummy store

--- noteful/src/App.js
+++ noteful/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {Route, Switch} from 'react-router-dom';
 
 
 function App() {
+
   return (
     <div className="App">
+    {/* <Route exact path="/" component={HomePage} /> */}
       <header className="App-header">
+      this is test app header
       </header>
     </div>
   );
